fe7f6d6156ad7747d4752367ae28f412c80a707b
Ryan Buttery <21200135+ryan-buttery@users.noreply.github.com>
2024-07-20 16:47:26 +0200

    created venv, added odt save functionality

--- inout.py
+++ inout.py
@@ -1,4 +1,6 @@
 import os
+from odf.opendocument import OpenDocumentText
+from odf.text import P, Span
 
 class inout:
     def __init__(self) -> None:
@@ -12,7 +14,7 @@ class inout:
         return 'This is a class of useful methods because I like my stuff tidy. Stop inspecting things and do something productive!'
 
     @classmethod
-    def read_txt(cls, filepath):
+    def read_txt(cls, filepath) -> None:
         """
         Class method that takes in a text file ready for further processing. This should not be a surprise if your IQ is above 20.
         Args:
@@ -27,7 +29,7 @@ class inout:
             print(f"You've got an error pal. Maybe your file doesn't exist, or you don't have permission to read it. Or you fat-fingered the filepath.\n{e}")
     
     @classmethod
-    def save_txt(cls, content):
+    def save_txt(cls, content) -> None:
         """
         Saves the transliterated text into a txt file, and appends '-futhorc' to the end.
 
@@ -35,12 +37,33 @@ class inout:
             filepath (str): Just give this the filepath var you used for the read_txt path. 
             content (str): The content you want to write to the txt file. Wow. Such original. Mucho amaze.
         """
-        cls.filepath
         cls.content = content
-        base, ext = os.path.splitext(cls.filepath)
-        new_filename = f"{base}-futhorc{ext}"
+        cls.base, cls.ext = os.path.splitext(cls.filepath)
+        cls.new_filename = f"{cls.base}-futhorc{cls.ext}"
         try:
-            with open(new_filename, 'w', encoding='utf-8') as file:
+            with open(cls.new_filename, 'w', encoding='utf-8') as file:
                 file.write(cls.content)
         except Exception as e:
             print(f"It's error time again! Maybe you don't have permission to write to the location... Whatever.\n{e}")
+    @classmethod
+    def save_as_odt(cls, content) -> None:
+        """
+        Saves the transliterated text into a. odt file, and appends '-futhorc' to the end.
+
+        Args:
+            filepath (str): Just give this the filepath var you used for the read_txt path. 
+            content (str): The content you want to write to the odt file. Wow. Such original. Mucho amaze.
+        """
+        cls.content = content
+        cls.base, cls.ext = os.path.splitext(cls.filepath)
+        cls.new_filename = f"{cls.base}-futhorc.odt"
+        try:
+            cls.lines = content.split('\n')
+            cls.doc = OpenDocumentText()
+            for line in cls.lines:
+                cls.p = P()
+                cls.p.addElement(Span(text=line))
+                cls.doc.text.addElement(cls.p)
+            cls.doc.save(cls.new_filename)
+        except Exception as e:
+            print(f"Uh oh. Something went wrong saving it as a libreoffice doc. Sorry pal:\n{e}")
